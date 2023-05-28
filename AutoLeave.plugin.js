2a81a9cf1f7d698b0108c6f9af8dc45df1e5d27f
sruusk <samuli.ruuskanen@live.fi>
2023-05-29 02:43:13 +0300

    Update AutoLeave.plugin.js

--- AutoLeave.plugin.js
+++ AutoLeave.plugin.js
@@ -1,1 +1,133 @@
+/**
+ * @name AutoLeave
+ * @author sruusk
+ * @authorLink https://github.com/sruusk
+ * @description Automatically leaves server after voice channel disconnect
+ * @website https://github.com/sruusk/BDPlugins
+ * @source https://raw.githubusercontent.com/sruusk/BDPlugins/main/AutoLeave.plugin.js
+ * @version 0.0.1
+ */
 
+const config = {
+    "info": {
+        "name": "AutoLeave",
+        "authors": [{
+            "name": "sruusk",
+            "github_username": "sruusk"
+        }],
+        "version": "0.0.1",
+        "description": "Automatically leaves server after voice channel disconnect",
+        "github": "https://github.com/sruusk/BDPlugins",
+        "github_raw": "https://raw.githubusercontent.com/sruusk/BDPlugins/main/AutoLeave.plugin.js"
+    },
+    "changelog": [
+
+    ],
+    "main": "index.js"
+};
+
+class Dummy {
+    constructor(meta) {
+        this.meta = meta;
+        this._config = config;
+    }
+    start() {}
+    stop() {}
+}
+
+if (!global.ZeresPluginLibrary) {
+    BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${ config.info.name } is missing. Please click Download Now to install it.`, {
+        confirmText: "Download Now",
+        cancelText: "Cancel",
+        onConfirm: () => {
+            require("request").get("https://betterdiscord.app/gh-redirect?id=9", async (err, resp, body) => {
+                if (err) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                if (resp.statusCode === 302) {
+                    require("request").get(resp.headers.location, async (error, response, content) => {
+                        if (error) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), content, r));
+                    });
+                }
+                else {
+                    await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+                }
+            });
+        }
+    });
+}
+
+module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
+    const guilds = [];
+
+    const plugin = (Plugin, Library) => {
+
+        let dirtyDispatch = BdApi.findModuleByProps("dispatch", "subscribe");
+        if (!dirtyDispatch) console.error("[PLUGIN] AutoLeave : Dispatch Module not found")
+
+        function ON_GUILD_JOINED(data){
+            console.log("Joined new guild", data);
+            const close = BdApi.showNotice(
+                "Leave server after voice disconnect?",
+                {
+                    type: "info",
+                    buttons: [
+                        {
+                            label: "No",
+                            onClick: () => {
+                                close();
+                            }
+                        },
+                        {
+                            label: "Yes",
+                            onClick: () => {
+                                console.log("Leaving Guild when user disconnects from voice channel", data.guild.id);
+                                guilds.push(data.guild.id);
+                                close();
+                            }
+                        }
+                    ],
+                    timeout: 10000
+                }
+            );
+        }
+
+        const awaitingLeave = {};
+        function ON_VOICE_STATE_UPDATE(data){
+            //console.log("VOICE_STATE_UPDATE", data);
+            if(!data?.voiceStates?.length) return;
+            const { channelId, guildId } = data?.voiceStates[0];
+            if(!channelId) { // User left voice channel
+                //console.log("User left voice channel");
+                if(guilds.includes(guildId)){
+                    console.log("User left voice channel in watched guild - leaving guild in 5 seconds");
+                    awaitingLeave[guildId] = setTimeout(() => {
+                        console.log("Leaving guild", guildId);
+                        BdApi.findModuleByProps("leaveGuild").leaveGuild(guildId);
+                        guilds.splice(guilds.indexOf(guildId), 1);
+                        delete awaitingLeave[guildId];
+                    }, 5000);
+                }
+            } else { // User joined voice channel
+                if(awaitingLeave[guildId]){
+                    console.log("User joined voice channel in watched guild before leaving guild. Cancelling leave.");
+                    clearTimeout(awaitingLeave[guildId]);
+                    delete awaitingLeave[guildId];
+                }
+            }
+        }
+
+        return class AutoLeave extends Plugin {
+            onStart() {
+                dirtyDispatch.subscribe("GUILD_CREATE", ON_GUILD_JOINED);
+                dirtyDispatch.subscribe("VOICE_STATE_UPDATES", ON_VOICE_STATE_UPDATE);
+            }
+
+            onStop() {
+                dirtyDispatch.unsubscribe("GUILD_CREATE", ON_GUILD_JOINED);
+                dirtyDispatch.unsubscribe("VOICE_STATE_UPDATES", ON_VOICE_STATE_UPDATE);
+            }
+        };
+
+    };
+    return plugin(Plugin, Api);
+})(global.ZeresPluginLibrary.buildPlugin(config));
