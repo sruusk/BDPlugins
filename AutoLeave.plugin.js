98425e363bf68ddee476cfb40396dae4ac33c4bc
sruusk <samuli.ruuskanen@live.fi>
2023-06-03 18:22:38 +0300

    Update AutoLeave.plugin.js

--- AutoLeave.plugin.js
+++ AutoLeave.plugin.js
@@ -8,162 +8,140 @@
  * @version 0.0.1
  */
 
-const config = {
-    info: {
-        name: "AutoLeave",
-        authors: [{
-            name: "sruusk",
-            github_username: "sruusk"
-        }],
-        version: "0.0.1",
-        description: "Automatically leaves server after voice channel disconnect",
-        github: "https://github.com/sruusk/BDPlugins",
-        github_raw: "https://raw.githubusercontent.com/sruusk/BDPlugins/main/AutoLeave.plugin.js"
-    },
-    changelog: [
+const defaultSettings = {
+    defaultLeave: true
+}
 
-    ],
-    main: "index.js",
-    defaultConfig: [
-        {
-            type: "switch",
-            id: "defaultLeave",
-            name: "Leave as default",
-            note: "Leave all new servers as default. If disabled you will be asked every time you join a new server.",
-            value: true
-        }
-    ]
-};
+const guilds = [];
+const awaitingLeave = {};
+let close;
+const dirtyDispatch = BdApi.findModuleByProps("dispatch", "subscribe");
+if (!dirtyDispatch) console.error("[PLUGIN] AutoLeave : Dispatch Module not found");
 
-class Dummy {
-    constructor(meta) {
-        this.meta = meta;
-        this._config = config;
+const setSetting = (id, value) => {
+    const settings = BdApi.loadData("AutoLeave", "settings", defaultSettings);
+    settings[id] = value;
+    BdApi.saveData("AutoLeave", "settings", settings);
+}
+
+const getSetting = (id) => {
+    const settings = BdApi.loadData("AutoLeave", "settings", defaultSettings);
+    return settings[id];
+}
+
+const ON_GUILD_JOINED = data => {
+    console.log("Joined new guild", data);
+    if(getSetting("defaultLeave")){
+        console.log("Leaving Guild when user disconnects from voice channel", data.guild.id);
+        guilds.push(data.guild.id);
+        close = BdApi.showNotice(
+            "Leaving server after voice disconnect!",
+            {
+                type: "warning",
+                buttons: [
+                    {
+                        label: "Cancel",
+                        onClick: () => {
+                            guilds.splice(guilds.indexOf(data.guild.id), 1);
+                            clearTimeout(awaitingLeave[data.guild.id]);
+                            delete awaitingLeave[data.guild.id];
+                            BdApi.showToast("Cancelled auto leave", {type: "success"});
+                            close();
+                        }
+                    }
+                ]
+            }
+        );
+    } else {
+        close = BdApi.showNotice(
+            "Leave server after voice disconnect?",
+            {
+                type: "info",
+                buttons: [
+                    {
+                        label: "No",
+                        onClick: () => {
+                            close();
+                        }
+                    },
+                    {
+                        label: "Yes",
+                        onClick: () => {
+                            console.log("Leaving Guild when user disconnects from voice channel", data.guild.id);
+                            guilds.push(data.guild.id);
+                            BdApi.showToast("Leaving server after voice disconnect", {type: "success"});
+                            close();
+                        }
+                    }
+                ],
+                timeout: 10000
+            }
+        );
     }
-    start() {}
-    stop() {}
 }
 
-if (!global.ZeresPluginLibrary) {
-    BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${ config.info.name } is missing. Please click Download Now to install it.`, {
-        confirmText: "Download Now",
-        cancelText: "Cancel",
-        onConfirm: () => {
-            require("request").get("https://betterdiscord.app/gh-redirect?id=9", async (err, resp, body) => {
-                if (err) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
-                if (resp.statusCode === 302) {
-                    require("request").get(resp.headers.location, async (error, response, content) => {
-                        if (error) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
-                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), content, r));
-                    });
-                }
-                else {
-                    await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
-                }
-            });
+const ON_VOICE_STATE_UPDATE = data => {
+    console.log("VOICE_STATE_UPDATE", data);
+    if(!data?.voiceStates?.length) return;
+    const { channelId, guildId } = data?.voiceStates[0];
+    if(!channelId) { // User left voice channel
+        if(guilds.includes(guildId)){
+            console.log("User left voice channel in watched guild - leaving guild in 5 seconds");
+            awaitingLeave[guildId] = setTimeout(() => {
+                console.log("Leaving guild", guildId);
+                BdApi.findModuleByProps("leaveGuild").leaveGuild(guildId);
+                guilds.splice(guilds.indexOf(guildId), 1);
+                delete awaitingLeave[guildId];
+                if(close) close();
+            }, 5000);
         }
+    } else { // User joined voice channel
+        if(awaitingLeave[guildId]){
+            console.log("User joined voice channel in watched guild before leaving guild. Cancelling leave.");
+            clearTimeout(awaitingLeave[guildId]);
+            delete awaitingLeave[guildId];
+        }
+    }
+}
+
+const createSwitch = (settingId, labelText) => {
+    // Create a container for the switch and label
+    const container = document.createElement("div");
+    container.style = "display: flex; align-items: center; justify-content: space-between;";
+
+    // Create the label for the switch
+    const label = document.createElement("label");
+    label.innerText = labelText;
+    label.style = "margin-left: 10px; color: white; font-size: 16px; font-weight: 500;";
+    container.appendChild(label);
+
+    // Clone the switch element from BetterDiscord
+    const switchElement = document.querySelector(".bd-switch").cloneNode(true);
+    switchElement.classList.remove("bd-switch-checked");
+    switchElement.querySelector("input").checked = getSetting(settingId);
+    switchElement.querySelector("input").addEventListener("change", () => {
+        setSetting(settingId, switchElement.querySelector("input").checked);
     });
+    container.appendChild(switchElement);
+
+    return container;
 }
 
-module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
-    const guilds = [];
-    const awaitingLeave = {};
-
-    const plugin = (Plugin, Library) => {
-        const {PluginUtilities} = Library;
-        let dirtyDispatch = BdApi.findModuleByProps("dispatch", "subscribe");
-        if (!dirtyDispatch) console.error("[PLUGIN] AutoLeave : Dispatch Module not found")
-
-        function ON_GUILD_JOINED(data){
-            //console.log("Joined new guild", data);
-
-            let settings = PluginUtilities.loadSettings(config.info.name);
-            if(settings.defaultLeave){
-                //console.log("Leaving Guild when user disconnects from voice channel", data.guild.id);
-                guilds.push(data.guild.id);
-                const close = BdApi.showNotice(
-                    "Leaving server after voice disconnect!",
-                    {
-                        type: "error",
-                        buttons: [
-                            {
-                                label: "Cancel",
-                                onClick: () => {
-                                    guilds.splice(guilds.indexOf(data.guild.id), 1);
-                                    clearTimeout(awaitingLeave[data.guild.id]);
-                                    delete awaitingLeave[data.guild.id];
-                                    close();
-                                }
-                            }
-                        ]
-                    }
-                );
-            } else {
-                const close = BdApi.showNotice(
-                    "Leave server after voice disconnect?",
-                    {
-                        type: "info",
-                        buttons: [
-                            {
-                                label: "No",
-                                onClick: () => {
-                                    close();
-                                }
-                            },
-                            {
-                                label: "Yes",
-                                onClick: () => {
-                                    //console.log("Leaving Guild when user disconnects from voice channel", data.guild.id);
-                                    guilds.push(data.guild.id);
-                                    close();
-                                }
-                            }
-                        ],
-                        timeout: 10000
-                    }
-                );
-            }
-        }
-        function ON_VOICE_STATE_UPDATE(data){
-            //console.log("VOICE_STATE_UPDATE", data);
-            if(!data?.voiceStates?.length) return;
-            const { channelId, guildId } = data?.voiceStates[0];
-            if(!channelId) { // User left voice channel
-                if(guilds.includes(guildId)){
-                    //console.log("User left voice channel in watched guild - leaving guild in 5 seconds");
-                    awaitingLeave[guildId] = setTimeout(() => {
-                        //console.log("Leaving guild", guildId);
-                        BdApi.findModuleByProps("leaveGuild").leaveGuild(guildId);
-                        guilds.splice(guilds.indexOf(guildId), 1);
-                        delete awaitingLeave[guildId];
-                    }, 5000);
-                }
-            } else { // User joined voice channel
-                if(awaitingLeave[guildId]){
-                    //console.log("User joined voice channel in watched guild before leaving guild. Cancelling leave.");
-                    clearTimeout(awaitingLeave[guildId]);
-                    delete awaitingLeave[guildId];
-                }
-            }
-        }
-
-        return class AutoLeave extends Plugin {
-            onStart() {
-                dirtyDispatch.subscribe("GUILD_CREATE", ON_GUILD_JOINED);
-                dirtyDispatch.subscribe("VOICE_STATE_UPDATES", ON_VOICE_STATE_UPDATE);
-            }
-
-            onStop() {
-                dirtyDispatch.unsubscribe("GUILD_CREATE", ON_GUILD_JOINED);
-                dirtyDispatch.unsubscribe("VOICE_STATE_UPDATES", ON_VOICE_STATE_UPDATE);
-            }
-
-            getSettingsPanel() {
-                const panel = this.buildSettingsPanel();
-                return panel.getElement();
-            }
-        };
-
-    };
-    return plugin(Plugin, Api);
-})(global.ZeresPluginLibrary.buildPlugin(config));
+module.exports = meta => ({
+    start() {
+        console.log("AutoLeave started", meta);
+        dirtyDispatch.subscribe("GUILD_CREATE", ON_GUILD_JOINED);
+        dirtyDispatch.subscribe("VOICE_STATE_UPDATES", ON_VOICE_STATE_UPDATE);
+    },
+    stop() {
+        dirtyDispatch.unsubscribe("GUILD_CREATE", ON_GUILD_JOINED);
+        dirtyDispatch.unsubscribe("VOICE_STATE_UPDATES", ON_VOICE_STATE_UPDATE);
+    },
+    getSettingsPanel() {
+        // Create a container for the settings
+        const container = document.createElement("div");
+        container.style = "display: flex; flex-direction: column; width: 100%;";
+        container.appendChild(createSwitch("defaultLeave", "Leave as default"));
+        return container;
+    }
+});
